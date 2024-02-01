3e4ecca5274deb204c495074e597a76c035efd59
Arek <arexspeed95@gmail.com>
2024-02-01 12:29:08 +0100

    quiz routing, props, watch

---
+++ quizbase/src/components/Result.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+const { quizQuestionLength, numberOfCorrectAnserws } = defineProps(['quizQuestionLength', 'numberOfCorrectAnserws'])
+</script>
+
+<template>
+    <div class="results">
+        <p>Your results...</p>
+        <h1>{{ numberOfCorrectAnserws }}/{{ quizQuestionLength }}</h1>
+        <RouterLink to="/">Go back</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.result {
+    text-align: center;
+    padding: 100px 0;
+}
+
+p {
+    font-size: 25px;
+}
+
+h1 {
+    font-size: 80px;
+}
+</style>
